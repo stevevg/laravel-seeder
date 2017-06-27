0aa97151da850b64d9ac72bf4b558aa24ee031e9
Edmund Luong <edmundvmluong@gmail.com>
2017-06-26 22:58:42 -0400

    Moved the ensurePathExists() call after the ensureMigrationDoesntAlreadyExist() call so that directories are only created if the Seeder class is valid

--- src/LaravelSeeder/Migration/SeederMigrationCreator.php
+++ src/LaravelSeeder/Migration/SeederMigrationCreator.php
@@ -23,9 +23,25 @@ class SeederMigrationCreator extends MigrationCreator
      */
     public function create($name, $path, $table = null, $create = false)
     {
+        $this->ensureMigrationDoesntAlreadyExist($name);
         $this->ensurePathExists($path);
 
-        return parent::create($name, $path, $table, $create);
+        // First we will get the stub file for the migration, which serves as a type
+        // of template for the migration. Once we have those we will populate the
+        // various place-holders, save the file, and run the post create event.
+        $stub = $this->getStub($table, $create);
+
+        $this->files->put(
+            $path = $this->getPath($name, $path),
+            $this->populateStub($name, $stub, $table)
+        );
+
+        // Next, we will fire any hooks that are supposed to fire after a migration is
+        // created. Once that is done we'll be ready to return the full path to the
+        // migration file so it can be used however it's needed by the developer.
+        $this->firePostCreateHooks();
+
+        return $path;
     }
 
     /**

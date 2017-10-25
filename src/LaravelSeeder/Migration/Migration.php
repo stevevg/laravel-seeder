c9867813bb06f7ba28c7b55b81ecb2b4d97b1c14
Stefano Pallicca <stefano.pallicca@gmail.com>
2017-10-25 11:49:31 +0200

    Fix for "seed --pretend"

---
+++ src/LaravelSeeder/Migration/Migration.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+abstract class Migration extends Seeder
+{
+    /**
+    * The name of the database connection to use.
+    *
+    * @var string
+    */
+    protected $connection;
+
+    /**
+    * Get the migration connection name.
+    *
+    * @return string
+    */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
